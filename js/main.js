f3f8a2632f7be5f8cb1dd300bab354451464d07b
Web Time <>
2021-01-20 13:06:23 +0200

    custom select

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var colorSelect = new CustomSelect({
+    elem: 'custom'
+});
+
+
+$('#custom2').ddslick({
+    data: ddData,
+    defaultSelectedIndex: 2,
+    imagePosition: "left",
+    // imagePosition: "right"
+});
+var ddData;
